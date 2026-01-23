0ae47659f3f787604ac192cbc8ed2b15af12b428
ansh3108 <anshkumar9686@gmail.com>
2026-01-23 09:36:03 +0530

    add wallet connection and system account inspection

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { Connection, clusterApiUrl } from '@solana/web3.js'
+import { useEffect } from 'react'
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const { publicKey, connected } = useWallet()
+  const [systemAccount, setSystemAccount] = useState(null)
+  const [balance, setBalance] = useState(null)
+  const [accountInfo, setAccountInfo] = useState(null)
+
+  const connection = new Connection(clusterApiUrl('devnet'));
+
+  useEffect(() => {
+    if(!publicKey) return 
+
+    const fetchSystemAccount = async () => {
+      const info = await connection.getAccountInfo(publicKey)
+      const lamports = await connection.getBalance(publicKey)
+
+      setAccountInfo(info)
+      setBalance(lamports)
+    }
+
+    fetchSystemAccount()
+  }, [publicKey])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: '20px '}}>
+      <h1>Wallet State Visualizer</h1>
+
+      <WalletMultiButton />
+
+      {connected && accountInfo && (
+        <div style={{ marginTop: '20px' }}>
+          <h2>System Account</h2>
+
+          <p><strong>Account address:</strong>{publicKey.toBase58()}</p>
+          <p><strong>Owner Program:</strong>{accountInfo.owner.toBase58()}</p>
+          <p><strong>Lamports: </strong>{balance}</p>
+          <p><strong>SOL: </strong>{balance / 1_000_000_000}</p>
+          <p><strong>Data Size:</strong>{accountInfo.data.length} bytes</p>   
+        </div>
+      )}
+    </div>
   )
+
+
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import '@solana/wallet-adapter-react-ui/styles.css'
+import { clusterApiUrl } from '@solana/web3.js'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+
+const endpoint = clusterApiUrl('devnet');
+const wallets = [new PhantomWalletAdapter()];
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+  <ConnectionProvider endpoint={endpoint}>
+    <WalletProvider wallets={wallets} autoConnect={false}>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
+  </StrictMode>
+
 )
